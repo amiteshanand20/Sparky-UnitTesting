39aff0dd136b7f92affcad9cd2a45952042de506
amitesh <amiteshanand20@gmail.com>
2024-08-30 20:53:05 +0530

    Setup inheritance

--- Sparky/Customer.cs
+++ Sparky/Customer.cs
@@ -8,6 +8,7 @@ namespace Sparky
 {
     public class Customer
     {
+        public int OrderTotal { get; set; }
         public int Discount = 15;
         public string GreetMessage { get; set; }
         public string CombineNames(string firstName, string lastName)
@@ -20,5 +21,23 @@ namespace Sparky
             Discount = 20;
             return GreetMessage ;
         }
+
+        public CustomerType GetCustomerDetails()
+        {
+            if(OrderTotal < 100)
+            {
+                return new BasicCustomer();
+            }
+            return new PlatinumCustomer();
+        }
+    }
+    public class CustomerType 
+    { 
+    }
+    public class BasicCustomer : CustomerType 
+    { 
+    }
+    public class PlatinumCustomer : CustomerType 
+    { 
     }
 }
