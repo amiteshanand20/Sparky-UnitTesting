444ad99531ccc92a70ee7fd5b387e806e9bedd5e
amitesh <amiteshanand20@gmail.com>
2024-09-07 17:58:32 +0530

    Integration test with bank account class

---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+        [SetUp]
+        public void SetUp()
+        {
+            bankAccount = new BankAccount(new LogBook());
+        }
+        [Test]
+        public void Deposit_Add100_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+
+            Assert.That(result, Is.True);
+            Assert.That(bankAccount.GetBalance(), Is.EqualTo(100));
+        }
+    }
+}
